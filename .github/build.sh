48d3d6b3458dcb7afe8b9a4a7ab1553a3b9b99f6
Alexey Turulin <repakuku@icloud.com>
2024-01-03 16:13:00 -0500

    github actions added

---
+++ .github/build.sh
@@ -0,0 +1,10 @@
+curl -Ls https: install.tuist.io | bash
+tuist install 3.36.2
+tuist fetch
+tuist generate
+
+xcodebuild clean -quiet
+xcodebuild build-for-testing\
+    -workspace 'MdEditor.xcworkspace' \
+    -scheme 'MdEditor' \
+    -destination 'platform=iOS Simulator,name=iPhone 15 Pro'
