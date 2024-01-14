e498a1a41c7ff40bfc4166e4fb40f9879c022498
Alexey Turulin <repakuku@icloud.com>
2024-01-13 19:58:53 -0500

    task tests init

---
+++ Packages/TaskManagerPackage/Tests/TaskManagerPackageTests/RegularTaskTests.swift
@@ -0,0 +1,11 @@
+//
+//  RegularTaskTests.swift
+//  
+//
+//  Created by Alexey Turulin on 1/13/24.
+//
+
+import XCTest
+
+final class RegularTaskTests: XCTestCase {
+}
---
+++ Packages/TaskManagerPackage/Tests/TaskManagerPackageTests/Test Doubles/MockTaskManager.swift
@@ -0,0 +1,8 @@
+//
+//  MockTaskManager.swift
+//  
+//
+//  Created by Alexey Turulin on 1/13/24.
+//
+
+import Foundation
