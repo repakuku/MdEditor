f2f12b0745bd90b9d6814f957690095da405268a
Alexey Turulin <repakuku@icloud.com>
2024-01-16 18:24:18 -0500

    accessibility identifier

---
+++ Project/MdEditor/Shared/AccessibilityIdentifier.swift
@@ -0,0 +1,19 @@
+//
+//  AccessibilityIdentifier.swift
+//  MdEditor
+//
+//  Created by Alexey Turulin on 1/16/24.
+//  Copyright © 2024 repakuku. All rights reserved.
+//
+
+enum AccessibilityIdentifier: String {
+	case textFieldLogin
+	case textFieldPass
+	case buttonLogin
+}
+
+enum AccessibilityIdentifier2: String {
+	case textFieldLogin = "textFieldLogin"
+	case textFieldPass = "textFieldPass"
+	case buttonLogin = "buttonLogin"
+}
