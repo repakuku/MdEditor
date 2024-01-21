99610e3577e6a3b34a34dc1a2bbf8702b426751e
Alexey Turulin <repakuku@icloud.com>
2024-01-21 02:04:51 -0500

    fix

--- .github/test.sh
+++
@@ -1,6 +0,0 @@
-cd ./Project
-
-xcodebuild test-without-building \
-    -workspace 'MdEditor.xcworkspace' \
-    -scheme 'MdEditor' \
-    -destination 'platform=iOS Simulator,name=iPhone 14 Pro Max'
