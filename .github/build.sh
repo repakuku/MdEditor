95a0a9201746e544b27dfd8ef192cd760c124461
Alexey Turulin <repakuku@icloud.com>
2024-02-06 16:42:45 -0500

    ci xcode 15 + tuist warning fix

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,11 @@
 cd ./Project
-curl -Ls https: install.tuist.io | bash
-tuist install 3.36.2
+# curl -Ls https: install.tuist.io | bash
+# tuist install 3.36.2
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install mise
+mise install tuist
+
 tuist fetch
 tuist generate
 
@@ -8,4 +13,4 @@ xcodebuild clean -quiet
 xcodebuild build-for-testing \
     -workspace 'MdEditor.xcworkspace' \
     -scheme 'MdEditor' \
-    -destination 'platform=iOS Simulator,name=iPhone 14 Pro Max'
+    -destination 'platform=iOS Simulator,name=iPhone 15 Pro Max'
